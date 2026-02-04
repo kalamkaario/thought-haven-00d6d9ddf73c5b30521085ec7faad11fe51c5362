21ca9f77f26e0cb609e7d59ca091d97e9e0c97bc
kalamkaario <64572750+kalamkaario@users.noreply.github.com>
2026-02-04 17:02:40 +0530

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gzcgzchajyckooihwfja.supabase.co";
-const supabaseKey = "sb_publishable_uLeD_k6tf9GcrYYN0fVahQ_OlZxTzZj"; // your key
+
+// YOUR CORRECT ANON PUBLIC KEY
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxamVpcW14c2xobXZlbWFucW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MjcxODksImV4cCI6MjA4NTUwMzE4OX0.n45_z5_K1IjntKsoY9JsvSssPqfKxndFtTNpFX9I4l4";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
