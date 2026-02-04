fc41bb2f6529e8378787988d0bbbc7796955da7a
kalamkaario <64572750+kalamkaario@users.noreply.github.com>
2026-02-04 17:17:12 +0530

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sqjeiqmxslhmvemanqmg.supabase.co";
-
-// YOUR CORRECT ANON PUBLIC KEY
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxamVpcW14c2xobXZlbWFucW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MjcxODksImV4cCI6MjA4NTUwMzE4OX0.n45_z5_K1IjntKsoY9JsvSssPqfKxndFtTNpFX9I4l4";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: { persistSession: false },
