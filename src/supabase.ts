f1e412caeb81640b2f635859044f0767a377f700
kalamkaario <64572750+kalamkaario@users.noreply.github.com>
2026-02-04 17:06:54 +0530

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gzcgzchajyckooihwfja.supabase.co";
 
-// YOUR CORRECT ANON PUBLIC KEY
+// 👇 PASTE YOUR ANON PUBLIC KEY HERE (from the bottom screenshot)
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxamVpcW14c2xobXZlbWFucW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MjcxODksImV4cCI6MjA4NTUwMzE4OX0.n45_z5_K1IjntKsoY9JsvSssPqfKxndFtTNpFX9I4l4";
 
